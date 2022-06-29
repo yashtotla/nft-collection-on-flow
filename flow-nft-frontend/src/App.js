5dfe3494041d9fd3eef2309a547accd21f93495e
yashtotla <yashtotla>
2022-06-29 11:22:08 +0530

    :lipstick: React app starter code for NFT mint website

---
+++ flow-nft-frontend/src/App.js
@@ -0,0 +1,35 @@
+//importing required libraries
+import React, { useState, useEffect } from "react";
+import './App.css';
+import twitterLogo from "./assets/twitter-logo.svg";
+
+import * as fcl from "@onflow/fcl";
+import * as types from "@onflow/types";
+
+const TWITTER_HANDLE = "holytotla";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <div className="logo-container">
+            <img src="./logo.png" className="flow-logo" alt="flow logo"/>
+            <p className="header">✨Awesome NFTs on Flow ✨</p>
+          </div>
+
+          <p className="sub-text">The easiest NFT mint experience ever!</p>
+        </div>
+
+        <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
